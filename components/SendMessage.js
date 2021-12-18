d8b40bc2a90518eecd6818aeed6321c2814cb7ba
spreadzp <spread2009@gmail.com>
2021-12-18 21:46:37 +0200

    day 3

---
+++ components/SendMessage.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import { useMoralis } from 'react-moralis'
+
+function SendMessage({ endOfMessagesRef }) {
+    const { user, Moralis } = useMoralis()
+    const [message, setMessage] = useState("")
+    const sendMessage = (e) => {
+        e.preventDefault()
+        if (!message) return
+        const Messages = Moralis.Object.extend('Messages')
+        const messages = new Messages()
+        messages.set("message", message)
+        messages.save({
+            message: message,
+            username: user.getUsername(),
+            ethAddress: user.get("ethAddress")
+        }).then(
+            (message) => {
+
+            }, (error) => {
+                console.log(error.message)
+            }
+        )
+        endOfMessagesRef.current.scrollIntoView({ behavior: "smooth" })
+        setMessage("")
+    }
+    return (
+        <form className="flex fixed bottom-10 bg-black opacity-80 w-1112
+         12 px-6 py-4 max-w-2xl shadow-xl rounded-full border-4 border-blue-400">
+            <input className="outline-none bg-transparent text-white placeholder-gray-500 pr-5" type="text"
+                placeholder={`Enter a Message ${user.getUsername()}...`} value={message}
+                onChange={e => setMessage(e.target.value)}
+            />
+            <button className="font-bold text-pink-500" type="submit" onClick={sendMessage}> Send</button>
+        </form>
+    )
+}
+
+export default SendMessage
